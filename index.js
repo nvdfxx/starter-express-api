307cfffa7daa6ab6aee405c7465e42696b094431
nvdfxx <artem7565@gmail.com>
2022-12-07 23:20:12 +0400

    api removed

--- index.js
+++ index.js
@@ -7,17 +7,15 @@ app.all('/', (req, res) => {
 
 const paladinsJS = require('paladins.js')
 
-const api = new paladinsJS.API({
-    devId: process.env.DEV_ID,
-    authKey: process.env.AUTH_KEY
-});
+// const api = new paladinsJS.API({
+//     devId: process.env.DEV_ID,
+//     authKey: process.env.AUTH_KEY
+// });
 
 
 
 app.get('/test', (req, res) => {
-
     return api.getPlayer('Tzzunami').then(r => res.send(r))
-
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('started'))
